648e71d50954ca757577cad1d0f194499f9c800d
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-03-12 11:11:20 +0100

    Update script.js
    Created functions buyHealth, buyWeapon, goTown

--- script.js
+++ script.js
@@ -43,3 +43,15 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon.");
 }
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
+}
+
+function goTown() {
+    
+}
