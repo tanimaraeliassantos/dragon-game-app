cb8e060856965918025c3582c8cb8b32ced73a8d
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-03-12 12:37:37 +0100

    Update script.js
    Added function update and removed content of previous functions as changes will be managed by function update now

--- script.js
+++ script.js
@@ -42,7 +42,8 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function goTown() {
+function update(location) {
+
 	button1.innerText = 'Go to store';
 	button2.innerText = 'Go to cave';
 	button3.innerText = 'Fight dragon';
@@ -52,15 +53,11 @@ function goTown() {
 	text.innerText =
 		'You are in the town square. You see a sign that says "Store".';
 }
+function goTown() {
+}
 
 function goStore() {
-	button1.innerText = 'Buy 10 health (10 gold)';
-	button2.innerText = 'Buy weapon (30 gold)';
-	button3.innerText = 'Go to town square';
-	button1.onclick = buyHealth;
-	button1.onclick = buyWeapon;
-	button3.onclick = goTown;
-	text.innerText = 'You enter the store.';
+	
 }
 
 function goCave() {
