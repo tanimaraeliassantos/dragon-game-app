19c7beb44e44f753f0bb62e6c656a54fd3a0edb4
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-03-12 10:50:26 +0100

    Update script.js
    Changed text when clicking on buttons 2 and 3

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ button3.onclick = fightDragon;
 function goStore() {
 	const button1 = document.querySelector('#button1');
     button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
 }
 
 function goCave() {
