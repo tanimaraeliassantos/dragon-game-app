21c197465cdfef306c98fa5d6c3a17022890114e
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-03-12 11:50:13 +0100

    Update script.js
    Added empty object to array

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterSName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
-const locations = [];
+const locations = [{}];
 
 //initialize buttons
 
