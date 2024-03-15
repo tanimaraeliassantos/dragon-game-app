f47c445f720a3adf41e17a0823207f58894524ce
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-03-15 13:25:27 +0100

    Update script.js
    Added text to let users know they don't have enough gold to buy health

--- script.js
+++ script.js
@@ -79,7 +79,9 @@ function buyHealth() {
 		health += 10;
 		goldText.innerText = gold;
 		healthText.innerText = health;
-	} else {}
+	} else {
+		text.innerText = 'You do not have enough gold to buy health.';
+	}
 }
 
 function buyWeapon() {}
