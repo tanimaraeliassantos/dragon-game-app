76c1d6acaeb30da43e30e0615d89461637755e11
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-03-15 13:34:44 +0100

    Update script.js
    Added four objects to variable weapons

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterSName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
-const weapons = [];
+const weapons = [
+	{ name: 'stick', power: 5 },
+	{ name: 'dagger', power: 30 },
+	{ name: 'claw hammer', power: 50 },
+	{ name: 'sword', power: 100 },
+];
 const locations = [
 	{
 		name: 'town square',
