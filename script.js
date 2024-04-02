736e27cd8b2778d2ba5f994fa98762dfda94a00f
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-02 15:06:09 +0200

    Update script.js
    Added health to inner text of health text
    Added monster health to inner text of monster health text

--- script.js
+++ script.js
@@ -169,6 +169,8 @@ function attack() {
 		' You attack it with your ' + weapons[currentWeapon].name + '.';
 		health -= monsters[fighting].level;
 		monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp)+ 1;
+		healthText.innerText = health;
+		monsterHealthText.innerText = monsterHealth;
 }
 
 function dodge() {}
