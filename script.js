423f95ed3bd4de4f19eec8766571d23e6551f5ca
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-02 15:26:19 +0200

    Update script.js
    Added text to dodge function

--- script.js
+++ script.js
@@ -179,7 +179,10 @@ function attack() {
 	}
 }
 
-function dodge() {}
+function dodge() {
+	text.innerText = "You dodge the attack from the " + monsters[fighting].name
+;
+}
 
 function defeatMonster() {}
 
