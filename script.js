b635d38b97a9aa139365d997e763bbce366c3a53
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-09 14:07:12 +0200

    Update script.js
    If player loses health and it's less than 0, then lose function will be updated

--- script.js
+++ script.js
@@ -276,16 +276,19 @@ function pick(guess) {
 	}
 	text.innerText = 'You picked ' + guess + '. Here are the random numbers:\n';
 	for (let i = 0; i < 10; i++) {
-		text.innerText += numbers[i] + "\n";
+		text.innerText += numbers[i] + '\n';
 	}
 	if (numbers.includes(guess)) {
-		text.innerText += "Right! You win 20 gold!";
-		gold +=20;
+		text.innerText += 'Right! You win 20 gold!';
+		gold += 20;
 		goldText.innerText = gold;
 	} else {
-		text.innerText += "Wrong! You lose 10 health!";
+		text.innerText += 'Wrong! You lose 10 health!';
 		health -= 10;
 		healthText.innerText = health;
+		if (health <= 0) {
+			lose();
+		}
 	}
 }
 
