4bea61147380b4afef91241090ebdeb0243aac1e
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-09 14:08:22 +0200

    Update script.js
    made hidden feature available

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ const locations = [
 	{
 		name: 'easter egg',
 		'button text': ['2', '8', 'Go to town square?'],
-		'button functions': [pickTwo, pickEight, goTown],
+		'button functions': [pickTwo, pickEight, easterEgg],
 		text: 'You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!',
 	},
 ];
