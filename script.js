898fcca2f928f746434f505c5edd046149316cc7
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-09 12:35:43 +0200

    Update script.js
    Changed game message to show numbers picked

--- script.js
+++ script.js
@@ -270,10 +270,11 @@ function easterEgg() {
 }
 
 function pick(guess) {
-	const numbers=[];
-	while(numbers.length < 10) {
-		numbers.push(Math,floor(Math.random() * 11));
+	const numbers = [];
+	while (numbers.length < 10) {
+		numbers.push(Math, floor(Math.random() * 11));
 	}
+	text.innerText = 'You picked ' + guess + '. Here are the random numbers:';
 }
 
 function pickTwo() {
